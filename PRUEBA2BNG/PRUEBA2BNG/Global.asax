e1f8b9caf78cec0ac7831f5fe5a176dd8f0485c1
alumnos <alumnos@DESKTOP-UNRBG16>
2016-11-09 18:46:50 -0600

    PRUEBA

---
+++ PRUEBA2BNG/PRUEBA2BNG/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PRUEBA2BNG.MvcApplication" Language="C#" %>
---
+++ PRUEBA2BNG/PRUEBA2BNG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRUEBA2BNG.Startup))]
+namespace PRUEBA2BNG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
